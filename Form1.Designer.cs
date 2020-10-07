036da1537a7a2d58aaffc0f19558576bfa8ae922
Namteleas <juan.flores@alumnos.udg.mx>
2020-10-06 22:36:56 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AnalizadorLexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAnalisis = new System.Windows.Forms.TextBox();
+            this.buttonAnalizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewLexico = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLexico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxAnalisis
+            // 
+            this.textBoxAnalisis.Location = new System.Drawing.Point(25, 45);
+            this.textBoxAnalisis.Name = "textBoxAnalisis";
+            this.textBoxAnalisis.Size = new System.Drawing.Size(521, 20);
+            this.textBoxAnalisis.TabIndex = 0;
+            // 
+            // buttonAnalizar
+            // 
+            this.buttonAnalizar.Location = new System.Drawing.Point(471, 94);
+            this.buttonAnalizar.Name = "buttonAnalizar";
+            this.buttonAnalizar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnalizar.TabIndex = 1;
+            this.buttonAnalizar.Text = "Analizar";
+            this.buttonAnalizar.UseVisualStyleBackColor = true;
+            this.buttonAnalizar.Click += new System.EventHandler(this.buttonAnalizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cadena a analizar";
+            // 
+            // dataGridViewLexico
+            // 
+            this.dataGridViewLexico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLexico.Location = new System.Drawing.Point(25, 80);
+            this.dataGridViewLexico.Name = "dataGridViewLexico";
+            this.dataGridViewLexico.Size = new System.Drawing.Size(431, 284);
+            this.dataGridViewLexico.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 376);
+            this.Controls.Add(this.dataGridViewLexico);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAnalizar);
+            this.Controls.Add(this.textBoxAnalisis);
+            this.Name = "Form1";
+            this.Text = "Analizador Lexico";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLexico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAnalisis;
+        private System.Windows.Forms.Button buttonAnalizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewLexico;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalizadorLexico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Token
+        {
+            public string tipo { get; set; }
+            public int identificador { get; set; }
+            public string lexema { get; set; }
+        }
+
+        private void buttonAnalizar_Click(object sender, EventArgs e)
+        {
+            var tokens = new BindingList<Token>();
+            string cadena, lexema, token;
+            int i, estado, valor;
+
+            i = 0;
+            estado = 0;
+            cadena = textBoxAnalisis.Text;
+
+            while(i <= (cadena.Length - 1) && estado == 0)
+            {
+                lexema = "";
+                token = "";
+                valor = -1;
+                while(i <= (cadena.Length - 1) && estado != -1)
+                {
+                    if (estado == 0)
+                    {
+                        if (Char.IsWhiteSpace(cadena, i)) //ignorar espacios
+                            estado = 0;
+                        else if (Char.IsLetter(cadena, i) || cadena[i] == '_') //empieza id 
+                        {
+                            estado = 1;
+                            lexema += cadena[i];
+                            token = "id";
+                            valor = 1;
+                        }
+                        else if (Char.IsDigit(cadena, i)) //empieza constante
+                        {
+                            estado = 2;
+                            lexema += cadena[i];
+                            token = "constante";
+                            valor = 13;
+                        }
+                        else if (cadena[i] == ';') //Solo es un caracter por lo que no necesita estado propio
+                        {
+                            lexema += cadena[i];
+                            token = ";";
+                            valor = 2;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == ',') //Solo es un caracter por lo que no necesita estado propio
+                        {
+                            lexema += cadena[i];
+                            token = ",";
+                            valor = 3;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == '(') //Solo es un caracter por lo que no necesita estado propio
+                        {
+                            lexema += cadena[i];
+                            token = "(";
+                            valor = 4;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == ')') //Solo es un caracter por lo que no necesita estado propio
+                        {
+                            lexema += cadena[i];
+                            token = ")";
+                            valor = 5;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == '{') //Solo es un caracter por lo que no necesita estado propio
+                        {
+                            lexema += cadena[i];
+                            token = "{";
+                            valor = 6;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == '}') //Solo es un caracter por lo que no necesita estado propio
+                        {
+                            lexema += cadena[i];
+                            token = "}";
+                            valor = 7;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == '=') //Puede ser relacional o solo =
+                        {
+                            estado = 5;
+                            lexema += cadena[i];
+                            token = "=";
+                            valor = 8;
+                        }
+                        else if (cadena[i] == '<' || cadena[i] == '>') //Relacional
+                        {
+                            estado = 5;
+                            lexema += cadena[i];
+                            token = "opRelacional";
+                            valor = 17;
+                        }
+                        else if (cadena[i] == '!') //Es error a menos que siga un '='
+                        {
+                            estado = 5;
+                            lexema += cadena[i];
+                            token = "error";
+                            valor = -1;
+                        }
+                        else if (cadena[i] == '+' || cadena[i] == '-') //Solo un caracter no necesita estado
+                        {
+                            lexema += cadena[i];
+                            token = "opSuma";
+                            valor = 14;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == '*' || cadena[i] == '/') //No necesita estado
+                        {
+                            lexema += cadena[i];
+                            token = "opMultiplicacion";
+                            valor = 16;
+                            estado = -1;
+                        }
+                        else if (cadena[i] == '|' || cadena[i] == '&') //Estado opLogico
+                        {
+                            lexema += cadena[i];
+                            token = "error";
+                            valor = -1;
+                            estado = 6;
+                        }
+                        else if (cadena[i] == '$') //No necesita estado
+                        {
+                            lexema += cadena[i];
+                            token = "$";
+                            valor = 18;
+                            estado = -1;
+                        }
+                        else
+                        {
+                            lexema += cadena[i];
+                            token = "error";
+                            valor = -1;
+                            estado = -1;
+                        }
+                        i++;
+                    }
+                    else if (estado == 1)  //Estado del id
+                    {
+                        if (Char.IsLetter(cadena, i) || cadena[i] == '_' || Char.IsDigit(cadena, i))
+                        {
+                            estado = 1;
+                            lexema += cadena[i];
+                            i++;
+                        }
+                        else //se determina si es palabra reservada
+                        {
+                            estado = -1;
+                            if (lexema == "int" || lexema == "float" || lexema == "char" || lexema == "void")
+                            {
+                                token = "tipoDeDato";
+                                valor = 0;
+                            }
+                            else if (lexema == "if")
+                            {
+                                token = lexema;
+                                valor = 9;
+                            }
+                            else if (lexema == "while")
+                            {
+                                token = lexema;
+                                valor = 10;
+                            }
+                            else if (lexema == "return")
+                            {
+                                token = lexema;
+                                valor = 11;
+                            }
+                            else if (lexema == "else")
+                            {
+                                token = lexema;
+                                valor = 12;
+                            }
+                            else
+                            {
+                                token = "id";
+                                valor = 1;
+                            }
+                        }
+                    }
+                    else if (estado == 2) //estado de entero (constante)
+                    {
+                        if (Char.IsDigit(cadena, i))
+                        {
+                            estado = 2;
+                            lexema += cadena[i];
+                            i++;
+                        }
+                        else if (cadena[i] == '.') // puede ser real (constante)
+                        {
+                            estado = 3;
+                            lexema += cadena[i];
+                            i++;
+                            token = "error";
+                            valor = -1;
+                        }
+                        else
+                            estado = -1;
+                    }
+                    else if (estado == 3) // Estado de transicion entero a real
+                    {
+                        if (Char.IsDigit(cadena, i))
+                        {
+                            estado = 4;
+                            lexema += cadena[i];
+                            token = "constante";
+                            valor = 13;
+                            i++;
+                        }
+                        else
+                            estado = -1;
+
+                    }
+                    else if (estado == 4) // Estado de real (constante)
+                    {
+                        if (Char.IsDigit(cadena, i))
+                        {
+                            estado = 4;
+                            lexema += cadena[i];
+                            i++;
+                        }
+                        else
+                            estado = -1;
+                    }
+                    else if (estado == 5) //Estado relacional
+                    {
+                        if (cadena[i] == '=') //Si llegan a este estado (=,<,>,!) pueden recibir un '=' mas
+                        {
+                            lexema += cadena[i];
+                            token = "opRelacional"; // '=' se vuelve opRelacional
+                            valor = 17;
+                            i++;
+                        }
+                        estado = -1;    //Si no es un '=' se mantienen igual y sale
+                    }
+                    else if (estado == 6) //Estado opLogico
+                    {
+                        if (lexema[0] == cadena[i])
+                        {
+                            lexema += cadena[i];
+                            token = "opLogico";
+                            valor = 15;
+                            i++;
+                        }
+                        estado = -1;
+                    }
+                    else //NO DEBERIA DE ENTRAR NUNCA
+                        estado = -1;
+                } // fin while por lexema
+                estado = 0;
+                tokens.Add(new Token() { tipo = token, identificador = valor, lexema = lexema });
+            } //fin while que recorre la cadena
+            dataGridViewLexico.DataSource = tokens;
+        }
+    }
+}
